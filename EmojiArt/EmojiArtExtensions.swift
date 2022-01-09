56327f1c0a6df9aa2110e4a0fb0dfd5a4c7a5518
Nyanny <EvgenyNedosekin@gmail.com>
2022-01-09 04:32:34 +0300

    initial commit

---
+++ EmojiArt/EmojiArtExtensions.swift
@@ -0,0 +1,106 @@
+//
+// Created by Admin on 09.01.2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Collection where Element: Identifiable {
+    func firstIndex(matching element: Element) -> Self.Index? {
+        firstIndex(where: { $0.id == element.id })
+    }
+
+    func contains(matching element: Element) -> Bool {
+        self.contains(where: { $0.id == element.id })
+    }
+}
+
+extension URL {
+    var imageURL: URL {
+        for query in query?.components(separatedBy: "&") ?? [] {
+            let queryComponents = query.components(separatedBy: "=")
+            if queryComponents.count == 2 {
+                if queryComponents[0] == "imgurl", let url = URL(string: queryComponents[1].removingPercentEncoding ?? "") {
+                    return url
+                }
+            }
+        }
+        if isFileURL {
+            var url = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first
+            url = url?.appendingPathComponent(self.lastPathComponent)
+            if url != nil {
+                return url!
+            }
+        }
+        return baseURL ?? self
+    }
+}
+
+extension CGPoint {
+    static func -(lhs: Self, rhs: Self) -> CGSize {
+        CGSize(width: lhs.x - rhs.x, height: lhs.y - rhs.y)
+    }
+    static func +(lhs: Self, rhs: CGSize) -> CGPoint {
+        CGPoint(x: lhs.x + rhs.width, y: lhs.y + rhs.height)
+    }
+    static func -(lhs: Self, rhs: CGSize) -> CGPoint {
+        CGPoint(x: lhs.x - rhs.width, y: lhs.y - rhs.height)
+    }
+    static func *(lhs: Self, rhs: CGFloat) -> CGPoint {
+        CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
+    }
+    static func /(lhs: Self, rhs: CGFloat) -> CGPoint {
+        CGPoint(x: lhs.x / rhs, y: lhs.y / rhs)
+    }
+}
+
+extension Array where Element == NSItemProvider {
+    func loadObjects<T>(ofType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: NSItemProviderReading {
+        if let provider = first(where: { $0.canLoadObject(ofClass: theType) }) {
+            provider.loadObject(ofClass: theType) { object, error in
+                if let value = object as? T {
+                    DispatchQueue.main.async {
+                        load(value)
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+    func loadObjects<T>(ofType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: _ObjectiveCBridgeable, T._ObjectiveCType: NSItemProviderReading {
+        if let provider = first(where: { $0.canLoadObject(ofClass: theType) }) {
+            let _ = provider.loadObject(ofClass: theType) { object, error in
+                if let value = object {
+                    DispatchQueue.main.async {
+                        load(value)
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+    func loadFirstObject<T>(ofType theType: T.Type, using load: @escaping (T) -> Void) -> Bool where T: NSItemProviderReading {
+        loadObjects(ofType: theType, firstOnly: true, using: load)
+    }
+    func loadFirstObject<T>(ofType theType: T.Type, using load: @escaping (T) -> Void) -> Bool where T: _ObjectiveCBridgeable, T._ObjectiveCType: NSItemProviderReading {
+        loadObjects(ofType: theType, firstOnly: true, using: load)
+    }
+}
+
+extension GeometryProxy {
+    func convert(_ point: CGPoint, from coordinateSpace: CoordinateSpace) -> CGPoint {
+        let frame = self.frame(in: coordinateSpace)
+        return CGPoint(x: point.x - frame.origin.x, y: point.y - frame.origin.y)
+    }
+}
+
+extension EmojiArt.Emoji {
+    var fontSize: CGFloat {CGFloat(self.size)}
+    var location: CGPoint {CGPoint (x: CGFloat(x), y: CGFloat(y))}
+}
+
+extension Data {
+    var utf8: String? { String(data: self, encoding: .utf8)}
+}
